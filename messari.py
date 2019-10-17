ef3d3826a00de68ec6bdcb5c7d09aa46f49a052b
Max Taylor-Davies <maxtaylordaview@gmail.com>
2019-10-17 16:34:51 +0100

    removes useless data

--- messari.py
+++ messari.py
@@ -2,12 +2,10 @@ import json
 from six.moves import urllib
 
 def main():
-    # get market data
-    getData("https://data.messari.io/api/v1/markets", "./MessariData/MarketsData.json")
     # get asset data
-    getData("https://data.messari.io/api/v1/assets", "./MessariData/AssetData.json")
+    getJsonData("https://data.messari.io/api/v1/assets", "./MessariData/AssetData.json")
 
-def getData(url, outputFile):
+def getJsonData(url, outputFile):
     response = urllib.request.urlopen(url)
     data = json.load(response)
     with open(outputFile, "w+") as dest:
