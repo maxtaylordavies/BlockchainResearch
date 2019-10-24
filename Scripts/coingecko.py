9dfc6ec9e4046693e4b13acf10ce91efc6264819
Max Taylor-Davies <maxtaylordaview@gmail.com>
2019-10-24 13:17:11 +0100

    adds functionality for mining historical market data on 70 tokens from Coingecko API

--- Scripts/coingecko.py
+++ Scripts/coingecko.py
@@ -1,4 +1,8 @@
 from pycoingecko import CoinGeckoAPI
+from messari import json2Csv
+import json
+import time
+
 
 def main():
     # initialise an instance of the API client
@@ -10,11 +14,41 @@ def main():
     # find the ids of the coins we're interested in
     allCoins = api.get_coins_list()
     ids = getDesiredCoinIds(allCoins, symbols)
-    print(ids)
     
+    # get historical data on coins by id
+    for id in ids:
+        getHistoricalDataOnCoin(api, id)
+
+
+def getHistoricalDataOnCoin(api, id):
+    data = []
+    hist = api.get_coin_market_chart_by_id(id=id, vs_currency="usd", days="max")
+
+    prices = list(map(lambda x: x[1], hist["prices"]))
+    marketCaps = list(map(lambda x: x[1], hist["market_caps"]))
+    totalVolumes = list(map(lambda x: x[1], hist["total_volumes"]))
+    timestamps = list(map(lambda x: x[0], hist["prices"]))
+
+    for i in range(len(timestamps)):
+        data.append({
+            "Date": time.strftime("%d-%m-%Y", time.localtime(timestamps[i]/1000)),
+            "Price (USD)": prices[i],
+            "Market cap": marketCaps[i],
+            "Total volume": totalVolumes[i]
+        })
+
+    jsonPath = "../Data/CoingeckoData/JSON/"+id+".json"
+    csvPath = "../Data/CoingeckoData/CSV/"+id+".csv"
+
+    with open(jsonPath, "w+", encoding="utf-8") as dest:
+        json.dump(data, dest, ensure_ascii=False, indent=4)
+
+    json2Csv(jsonPath, csvPath=csvPath)    
+
 
 def getDesiredCoinIds(coins, symbols):
     return [c["id"] for c in list(filter(lambda x: x["symbol"] in symbols, coins))]
+
 
 def getDesiredCoinSymbols():
     return [
--- Scripts/messari.py
+++ Scripts/messari.py
@@ -13,7 +13,7 @@ def getJsonData(url, outputFile):
     with open(outputFile, "w+", encoding="utf-8") as dest:
         json.dump(data, dest, ensure_ascii=False, indent=4)
 
-def json2Csv(jsonFile, key=None):
+def json2Csv(jsonFile, key=None, csvPath=None):
     # open json file
     with open(jsonFile) as data:    
         data = json.load(data)
@@ -22,7 +22,8 @@ def json2Csv(jsonFile, key=None):
     df = json_normalize(data, key)
 
     # export to csv file
-    csvPath = jsonFile[:-4] + "csv"
+    if csvPath == None:
+        csvPath = jsonFile[:-4] + "csv"
     df.to_csv(csvPath)
 
 if __name__== "__main__":
